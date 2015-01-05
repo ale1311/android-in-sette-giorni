c45f135a3540e2c6f8b833bb7e73cf921bceedb5
Android in sette Giorni <dekra06@gmail.com>
2015-01-05 10:48:05 +0100

    Broadcast added

--- adt/cap3/src/com/androidinsettegiorni_cap3/fragment/EmptyFragment.java
+++ adt/cap3/src/com/androidinsettegiorni_cap3/fragment/EmptyFragment.java
@@ -1,16 +1,22 @@
 package com.androidinsettegiorni_cap3.fragment;
 
-import com.androidinsettegiorni_cap3.R;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.androidinsettegiorni_cap3.R;
+
 public class EmptyFragment extends Fragment {
-	
+
+	public EmptyFragment() {
+		super();
+
+	}
+
 	private int color;
-	
+
 	public EmptyFragment(int color) {
 		this.color = color;
 	}
@@ -19,7 +25,7 @@ public class EmptyFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle) {
 		View view = inflater.inflate(R.layout.empty_fragmen, container, false);
 		view.setBackgroundColor(color);
-		
+
 		return view;
 	}
 }
--- adt/cap3/src/com/androidinsettegiorni_cap3/fragment/LabelListFragment.java
+++ adt/cap3/src/com/androidinsettegiorni_cap3/fragment/LabelListFragment.java
@@ -2,22 +2,26 @@ package com.androidinsettegiorni_cap3.fragment;
 
 import java.util.ArrayList;
 
-import com.androidinsettegiorni_cap3.adapter.LabelListAdapter;
-import com.androidinsettegiorni_cap3.model.Label;
-
-
 import android.app.ListFragment;
 import android.os.Bundle;
 
+import com.androidinsettegiorni_cap3.adapter.LabelListAdapter;
+import com.androidinsettegiorni_cap3.model.Label;
+
 public class LabelListFragment extends ListFragment {
-	
-private ArrayList<Label> list;
+
+	public LabelListFragment() {
+		super();
+
+	}
+
+	private ArrayList<Label> list;
 	private LabelListAdapter adapter;
-	
+
 	public LabelListFragment(ArrayList<Label> list) {
 		this.list = list;
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,14 +30,14 @@ private ArrayList<Label> list;
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
+
 		setListAdapter(null);
-		
-		if (adapter == null) {		
+
+		if (adapter == null) {
 			adapter = new LabelListAdapter(getActivity(), list);
 		}
-		
+
 		setListAdapter(adapter);
 	}
-	
+
 }
