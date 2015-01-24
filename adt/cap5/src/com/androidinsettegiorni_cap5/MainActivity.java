efeed79a74d34117607a81f2138f85c315ff7ff7
Android in sette Giorni <dekra06@gmail.com>
2015-01-24 17:44:19 +0100

    Image download update

--- adt/cap5/src/com/androidinsettegiorni_cap5/MainActivity.java
+++ adt/cap5/src/com/androidinsettegiorni_cap5/MainActivity.java
@@ -1,17 +1,51 @@
 package com.androidinsettegiorni_cap5;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.androidinsettegiorni_cap5.task.ImageAsyncTask;
 import com.androidinsettegiorni_cap5.task.NetworkAsynckTask;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements ServiceConnection {
 
 	private TextView ipView, providerView, ispView, locationView;
+	private ImageView mImageView;
+
+	private Messenger mUrlService;
+
+	public static final int URL_ID = 333;
+
+	private Messenger mSelectedUrlMessenger = new Messenger(new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+
+			switch (msg.what) {
+			case URL_ID:
+				String url = (String) msg.obj;
+
+				new ImageAsyncTask(MainActivity.this, mImageView).execute(url);
+				break;
+
+			default:
+				break;
+			}
+
+			super.handleMessage(msg);
+		}
+	});
+	private boolean mBound;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,30 +56,60 @@ public class MainActivity extends Activity {
 		providerView = (TextView) findViewById(R.id.current_provider);
 		ispView = (TextView) findViewById(R.id.current_isp);
 		locationView = (TextView) findViewById(R.id.current_localtion);
+		mImageView = (ImageView) findViewById(R.id.image);
 
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 	public void calculate_ip(View v) {
 
 		new NetworkAsynckTask(this, ipView, providerView, ispView, locationView).execute();
+	}
+
+	public void update_image(View v) {
+
+		Message messageToService = new Message();
+		messageToService.replyTo = mSelectedUrlMessenger;
+		messageToService.what = ImageUrlService.MSG_URL_ID;
+		messageToService.arg1 = (int) System.currentTimeMillis();
+
+		try {
+			mUrlService.send(messageToService);
+		} catch (RemoteException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+		mBound = true;
+		mUrlService = new Messenger(arg1);
+
+	}
+
+	@Override
+	public void onServiceDisconnected(ComponentName arg0) {
+		mBound = false;
+		mUrlService = null;
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		/*
+		 * Start del service
+		 */
+		Intent i = new Intent(this, ImageUrlService.class);
+		bindService(i, this, Context.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		// Disaccoppiamento dal service
+		if (mBound) {
+			unbindService(this);
+			mBound = false;
+		}
 	}
 }
